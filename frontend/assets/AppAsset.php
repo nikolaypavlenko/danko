22ec3231679dccc1a903e1ba76a5e0f0d5cecf6b
nikolaypavlenko <nnik@bk.ru>
2016-07-01 13:52:42 +0300

    занятие 6: запросы SQL, определение id, подключение JS, CSS

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,8 +13,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        //добавление файла с CSS
     ];
     public $js = [
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- frontend/views/site/news.php
+++ frontend/views/site/news.php
@@ -2,38 +2,8 @@
 <?php foreach($news as $item) : ?>
 
 
-<h2 style="color:green"> Новость <?=$item->title?>  </h2>
+<a href="/frontend/web/index.php?r=site%2Ftest&id=<?=$item->id?>">  <h2 style="color:green"> Новость <?=$item->title?> </h2> </a>
 
-<p> <?=$item->text ?>    </p>
 
-<h5 style="font-style:italic">  <?=$item->create_date ?>   </h5> 
 
-<span style="font-weight:bold; font-size:15px"> <?=$item->user->username ?> </span>
-
-<h6 style="color:blue; font-style:italic"> <?=$item->tag->name_tag ?> </h6><br>
-
-<?php endforeach; ?>
-
-//////////////////////////////////////////////////////
-
-<?php foreach($users as $user) : ?>
-
-	<h6 style="font-style:italic"> <?=$user->username ?> </h6>
-
-		<?php foreach($user->news as $news): ?>
-				<?=$news->title ?> <br>
-		<?php endforeach; ?>
-
-<?php endforeach; ?>
-
-<br><br> //////////////////////////////////////////////
-
-<?php foreach($tags as $tag) : ?>
-
-	<h6 style="font-style:italic"> <?=$tag->name_tag ?> </h6>
-
-		<?php foreach($tag->news as $news): ?>
-				<?=$news->title ?> <br>
-		<?php endforeach; ?>
-
-<?php endforeach; ?>
+<?php endforeach; ?><hr>
